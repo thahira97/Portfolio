ccdd7034fbcac3cf47ce671e5e09dd5b9d793c0b
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 01:58:12 -0400

    Add Skills Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
-import {Banner} from "./components/Banner"
+import {Banner} from "./components/Banner";
+import { Skills } from './components/Skills';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
