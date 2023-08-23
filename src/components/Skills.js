b06b1c9c08c53f93c045e208683702f9ae53d71a
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 07:57:47 -0400

    Include meters in the image

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,10 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -37,19 +40,19 @@ export const Skills = () => {
           </p>
           <Carousel responsive={responsive} infinite={true} className='skill-slider'>
             <div className='item'>
-              <imr src={""} alt="Image" />
+              <imr src={meter1} alt="Image" />
                <h5>Web Development</h5>
             </div>
             <div className='item'>
-              <imr src={""} alt="Image" />
+              <imr src={meter2} alt="Image" />
                <h5>Figma Design</h5>
             </div>
             <div className='item'>
-              <imr src={""} alt="Image" />
+              <imr src={meter3} alt="Image" />
                <h5>Bootstrap</h5>
             </div>
             <div className='item'>
-              <imr src={""} alt="Image" />
+              <imr src={meter3} alt="Image" />
                <h5>Logo Design</h5>
             </div>
           </Carousel>
