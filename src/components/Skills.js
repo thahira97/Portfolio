7e9528e4f3bf9c0cde7c591039c2c56a5a0c8b86
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 08:46:56 -0400

    Upload html logo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png"
-
+import htmlLogo from "../assets/img/html.png"
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
@@ -56,6 +56,9 @@ export const Skills = () => {
                <h5>Logo Design</h5>
             </div>
           </Carousel>
+          <div className='stacks'>
+           <img src={htmlLogo} alt="html-logo"  />
+          </div>
         </div>
         </Col>
       </Row>
