a68c988d6dabe17af518dbd41b1a2c30df037c2b
thahira97 <thahirasheerin3@gmail.com>
2023-08-22 20:02:51 -0400

    Add bootstrap.min to size dwon the logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
