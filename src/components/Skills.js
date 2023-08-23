86396774fd92f6b701e8bff9f0031f2a5960fbe6
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 07:37:06 -0400

    Add Carousel to make it look like slide

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -27,6 +27,32 @@ export const Skills = () => {
     <Container>
       <Row>
         <Col>
+        <div className='skill-bx'>
+          <h2>
+            Skills
+          </h2>
+          <p>
+            Tech stacks that I have used
+          </p>
+          <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+            <div className='item'>
+              <imr src={""} alt="Image" />
+               <h5>Web Development</h5>
+            </div>
+            <div className='item'>
+              <imr src={""} alt="Image" />
+               <h5>Figma Design</h5>
+            </div>
+            <div className='item'>
+              <imr src={""} alt="Image" />
+               <h5>Bootstrap</h5>
+            </div>
+            <div className='item'>
+              <imr src={""} alt="Image" />
+               <h5>Logo Design</h5>
+            </div>
+          </Carousel>
+        </div>
         </Col>
       </Row>
     </Container>
