328af435f054e5f478fb81096bbd57bea832a14a
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 16:38:40 -0400

    Use react bootstrap to create tabs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+import {Container, Row, Col, Nav} from 'react-bootstrap'
 export const Projects = () => {
  const projects = [{
   title: "JobLeap",
@@ -13,7 +14,30 @@ export const Projects = () => {
   imageUrl: "../assets/img/interview-scheduler.png"
  }
 ]
-
-  return <></>;
+  return (
+    <section className="projects" id="project">
+    <Container>
+      <Row>
+        <Col>
+        <h2>Projects</h2>
+        <p>Checkout the my works</p>
+        <Nav variant="pills" defaultActiveKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+        </Col>
+      </Row>
+    </Container>
+    </section>
+  );
 };
  
