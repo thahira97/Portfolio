1380aa23dfa7f2c5242b28cbf4b97cd1d8b6948d
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 07:23:20 -0400

    Use template from multi carrousel

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,35 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 export const Skills = () => {
-  return <></>;
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <section className='skill' id="skills">
+    <Container>
+      <Row>
+        <Col>
+        </Col>
+      </Row>
+    </Container>
+    </section>
+  );
 };
