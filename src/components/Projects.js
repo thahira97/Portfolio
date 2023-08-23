95e5a58130b8ce5a114300bd9777fc2af64f9a89
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 18:20:03 -0400

    Add all the projects as cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,58 +1,52 @@
-import {Container, Row, Col, Nav, TabContainer, TabContent, TabPane} from 'react-bootstrap'
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Carousel,
+  ListGroup,
+} from "react-bootstrap";
+import jobleap from "../assets/img/jobleap.png";
+import maestro from "../assets/img/maestro.png";
+import interview from "../assets/img/interview-scheduler.png";
+
 export const Projects = () => {
- const projects = [{
-  title: "JobLeap",
-  description: "Resume Modification App",
-  imageUrl: "../assets/img/jobleap.png"
- },
- {title: "Maestro Pizza",
- description: "A food-ordering app",
- imageUrl: "../assets/img/maestro.png"
- },
- {title: "Interview-Scheduler",
-  description: "A real time interview booking app",
-  imageUrl: "../assets/img/interview-scheduler.png"
- }
-]
+  const projects = [
+    {
+      title: "JobLeap",
+      description: "Resume Modification App",
+      imageUrl: jobleap,
+    },
+    {
+      title: "Maestro Pizza",
+      description: "A food-ordering app",
+      imageUrl: maestro,
+    },
+    {
+      title: "Interview-Scheduler",
+      description: "A real time interview booking app",
+      imageUrl: interview,
+    },
+  ];
   return (
     <section className="projects" id="project">
-    <Container>
-      <Row>
-        <Col>
-        <h2>Projects</h2>
-        <p>Apps I have built</p>
-        <TabContainer id="projects-tabs" defaultActiveKey="first">
-        <Nav variant="pills" defaultActiveKey="/home">
-      <Nav.Item>
-        <Nav.Link eventKey="first">React projects</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="second">jQuery projects</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="third">
-          Ruby
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-    <TabContent>
-      <TabPane eventKey="first">
-       <Row>
-        {
-          projects.map((project, index) => {
-            return (
-              <p>{project.title}</p>
-            )
-          })
-        }
-       </Row>
-      </TabPane>
-    </TabContent>
-    </TabContainer>
-        </Col>
-      </Row>
-    </Container>
+      <Container>
+        <Row xs={1} md={2} className="g-4">
+          {projects.map((project, idx) => (
+            <Col key={idx}>
+              <Card>
+                <Card>
+                  <Card.Img variant="top" src={project.imageUrl} />
+                </Card>
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </section>
   );
 };
- 
