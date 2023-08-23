70cae04ed4ef018ecfca5fe73574b49d33f08aca
thahira97 <thahirasheerin3@gmail.com>
2023-08-23 16:20:30 -0400

    Add projects image and description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,17 @@ export const Projects = () => {
  const projects = [{
   title: "JobLeap",
   description: "Resume Modification App",
-  imageUrl: ""
- }]
+  imageUrl: "../assets/img/jobleap.png"
+ },
+ {title: "Maestro Pizza",
+ description: "A food-ordering app",
+ imageUrl: "../assets/img/maestro.png"
+ },
+ {title: "Interview-Scheduler",
+  description: "A real time interview booking app",
+  imageUrl: "../assets/img/interview-scheduler.png"
+ }
+]
 
   return <></>;
 };
