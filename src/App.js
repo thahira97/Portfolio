5ff2db3575fe197a4a126f2e9addd0aaab1e16a0
thahira97 <thahirasheerin3@gmail.com>
2023-08-22 20:06:01 -0400

    Add Banner component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
+import {Banner} from "./components/Banner"
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
