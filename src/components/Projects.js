23669a79c6fff496c004057b2933fce38e854a9f
thahira97 <thahirasheerin3@gmail.com>
2023-10-20 11:44:41 -0400

    Make changes in the css of project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,48 +1,52 @@
+import React from 'react';
 import {
   Container,
   Row,
   Col,
   Card,
-  Carousel,
-  ListGroup,
-} from "react-bootstrap";
-import jobleap from "../assets/img/jobleap.png";
-import maestro from "../assets/img/maestro.png";
-import interview from "../assets/img/interview-scheduler.png";
+} from 'react-bootstrap';
+import jobleap from '../assets/img/jobleap.png';
+import maestro from '../assets/img/maestro.png';
+import interview from '../assets/img/interview-scheduler.png';
 
 export const Projects = () => {
   const projects = [
     {
-      title: "JobLeap",
-      description: "Transforming job searches with personalized profiles and curated opportunities, featuring dynamic resume customization via integrated ChatGPT API.",
+      title: 'JobLeap',
+      description: `JobLeap is a cutting-edge application that revolutionizes the job search experience. With JobLeap, users can effortlessly create personalized profiles and instantly access a curated list of job opportunities that align with their specific criteria. Seamlessly applying for jobs is made even easier as our integrated ChatGPT API dynamically modifies resumes based on individual requirements. Whether opting for the original or modified version, JobLeap empowers users to confidently pursue their career aspirations.`,
+      techStack: "Express.js,React,Node.js,PostgreSQL,ChatGPT API",
       imageUrl: jobleap,
     },
     {
-      title: "Maestro Pizza",
-      description: "Twilio-powered app for real-time order updates, elevating the customer-restaurant experience with seamless tracking and tech integration.",
+      title: 'Maestro Pizza',
+      description: 'Twilio-powered app for real-time order updates, elevating the customer-restaurant experience with seamless tracking and tech integration.',
+      techStack: "Express.js,jQuery,Node.js,PostgreSQL,Twilio API",
       imageUrl: maestro,
     },
     {
-      title: "Interview-Scheduler",
-      description: "A real time interview booking app",
+      title: 'Interview-Scheduler',
+      description: `Interview Scheduler is a user-friendly, single-page application that facilitates the booking and management of mentor interviews for students.
+      This React-based front-end application employs Express on the back-end, with PostgreSQL database support from the scheduler-api. Robust testing was performed using Jest and Cypress to ensure seamless functionality.`,
+      techStack: "Express.js,React,Node.js,PostgreSQL",
       imageUrl: interview,
     },
   ];
+
   return (
     <section className="projects" id="project">
       <Container>
-           <h2>Projects</h2>
+        <h2>Projects</h2>
         <p>Apps that I've built</p>
         <Row xs={1} md={2} className="g-4">
           {projects.map((project, idx) => (
             <Col key={idx}>
-              <Card style={{width: "40rem"}}>
-                <Card >
-                  <Card.Img variant="top" src={project.imageUrl} />
-                </Card>
+              <Card className="project-card">
+                <Card.Img variant="top" src={project.imageUrl} />
                 <Card.Body>
                   <Card.Title>{project.title}</Card.Title>
-                  <Card.Text>{project.description}</Card.Text>
+                  <Card.Text>{project.description}
+                  <br></br>
+                  <b>TechStacks used: </b>{project.techStack}</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
