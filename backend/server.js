34a797ce9097ac33c0d833dc292896524b3256a1
thahira97 <thahirasheerin3@gmail.com>
2023-10-23 23:44:15 -0400

    Change port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-const PORT = 3003;
+const PORT = 5000;
+
+app.use("/", (req,res)=> {
+  res.send("Server is running")
+})
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
