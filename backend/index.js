8575a0f711e7e75f361a9c0fc5c0572605214763
thahira97 <thahirasheerin3@gmail.com>
2023-10-24 21:13:30 -0400

    Change port

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/", router);
 app.use(favicon(__dirname + '/favicon.ico'));
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.get("/", (req, res) => {
   res.send("Server is running");
