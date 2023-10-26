5d75b4b29f260d22af0027c2210bd0fd38acfef9
thahira97 <thahirasheerin3@gmail.com>
2023-10-26 10:58:31 -0400

    Fix cors

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -27,7 +27,7 @@ export const Contact = () => {
     setButtonText("Sending..");
     
     try {
-      const response = await fetch("http://localhost:8080/contact", {
+      const response = await fetch("https://portfolio-back-alpha.vercel.app/contact", {
         method: "POST",
         mode: "cors",
         headers: {
